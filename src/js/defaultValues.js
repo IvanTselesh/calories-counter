25dbe51eac6cdcc781e627cbe43160dfcb5e0d4f
Ivan <vano5964307@gmail.com>
2022-05-16 22:48:49 +0200

    feat: Completing of defaults values, switch of male/female and function of clear button

---
+++ src/js/defaultValues.js
@@ -0,0 +1,10 @@
+import { minAct, maleBtn, age, height, weight} from "./querySelectors";
+
+/* export function doDefault() {
+  age.innerText = '10';
+  height.textContent = 0;
+  weight.textContent = 0;
+
+}
+
+doDefault(); */
